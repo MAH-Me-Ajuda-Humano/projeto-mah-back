b389d0ef5eaddd4f695ee9042d5a6eab7ad3be3d
leorusso2300 <leorusso2607@gmail.com>
2021-11-09 14:34:32 -0300

    mah-back

---
+++ mah-projeto/src/main/java/com/generation/mahprojeto/MahProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.generation.mahprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahProjetoApplication.class, args);
+	}
+
+}
---
+++ mah-projeto/src/test/java/com/generation/mahprojeto/MahProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.mahprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
